933064254f869da9f86096793b38b028551eede3
sven <s.lepper22@gmail.com>
2024-04-26 17:08:57 +0200

    Revoked some features due to containerzing the software

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-APP_ENV = os.environ.get('APP_ENV', 'development')
-DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'admin')
-DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'admin')
-DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
-DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
-DATABASE_NAME = os.getenv('DATABASE_NAME', 'postgres')
--- backend/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from backend.api import login
-
-app = FastAPI(title="Inventory Database Backend")
-
-
-app.include_router(login.router)
