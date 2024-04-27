50bac16b513edc6c724d7802f4365f4e89b49703
slepp22 <102992553+slepp22@users.noreply.github.com>
2024-04-27 13:47:21 +0200

    sample pin api added

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -5,3 +7,8 @@ router = APIRouter()
 @router.get("/")
 async def read_root():
     return {"message": "Hello, World!"}
+
+@router.get("/pin")
+async def generate_pin():
+    pin = random.randint(1000, 9999)
+    return {pin}
